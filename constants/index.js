155f9c196f5ab8d546de991023b08a77e829c5ef
Quincy Fer <quincy.e.fer@gmail.com>
2024-08-16 17:39:39 +0200

    Data entry testimonials

--- constants/index.js
+++ constants/index.js
@@ -2,6 +2,7 @@ import { FaDumbbell, FaClipboardCheck, FaClock } from "react-icons/fa6";
 import { PiVirtualRealityFill } from "react-icons/pi";
 import { GrLounge, GrYoga } from "react-icons/gr";
 import { FaLocationArrow } from "react-icons/fa6";
+import { IoMdQuote } from "react-icons/io";
 
 export const NAVLINKS = [
   {
@@ -149,5 +150,96 @@ export const PRICING = [
       "Experience the ultimate in fitness with Ultimate Pulse, where personalized services and exclusive access meet to elevate your performance and results.",
     key: "ultimate",
     price: 85,
+  },
+];
+
+export const TESTIMONIALS = [
+  {
+    name: "Megan O'Connor",
+    key: "Megan",
+    text: "PulseFits state-of-the-art equipment has revolutionized my workouts. The variety of machines and free weights keeps every session fresh and challenging.\
+     Plus, the unlimited group classes with my Premium Membership keep me motivated and engaged!",
+    image: "/images/woman1.jpg",
+    icon: IoMdQuote,
+  },
+  {
+    name: "Carlos Ramirez",
+    key: "Carlos",
+    text: "The diverse range of classes at PulseFit ensures I never get bored. From high-energy HIIT to relaxing yoga, there's something for every mood and fitness level. The Elite Membership's \
+    unlimited personal training sessions have been a game-changer for my progress",
+    image: "/images/man1.jpg",
+    icon: IoMdQuote,
+  },
+  {
+    name: "Jasmine Xian",
+    key: "Jasmine",
+    text: "As someone new to fitness, PulseFit's personalized training plans and supportive trainers made all the difference. The Basic Membership's\
+     inclusion of a weekly free class was the perfect way to start my journey",
+    image: "/images/woman2.jpg",
+    icon: IoMdQuote,
+  },
+  {
+    name: "Ramesh Patel",
+    key: "Ethan",
+    text: "PulseFit's premium amenities, like the sauna and recovery lounge, are incredible. The Enhanced Access of my Premium \
+    Membership makes every visit feel like a treat. The priority booking for classes is also a huge plus!",
+    image: "/images/man2.jpg",
+    icon: IoMdQuote,
+  },
+  {
+    name: "Chloe Edwards",
+    key: "Chloe",
+    text: "The holistic approach at PulseFit, from the advanced recovery tools to the exclusive wellness workshops, is what sets it apart. My Elite Membership allows me access to all these benefits, making every workout session more effective and enjoyable",
+    image: "/images/woman3.jpg",
+    icon: IoMdQuote,
+  },
+  {
+    name: "Noah Sullivan",
+    key: "Noah",
+    text: "PulseFit's community vibe and high-energy classes have completely transformed my fitness routine. The diverse class offerings and premium facilities available with my membership keep me motivated and excited to come back every day",
+    image: "/images/man3.jpg",
+    icon: IoMdQuote,
+  },
+  {
+    name: "Sofia Martinez",
+    key: "Sofiaa",
+    text: "The variety of classes at PulseFit is amazing. Whether I'm doing a high-intensity workout or a relaxing yoga session, the facilities and expert trainers make each experience unique. The unlimited classes and personalized plan from my Premium Membership are invaluable",
+    image: "/images/woman4.jpg",
+    icon: IoMdQuote,
+  },
+  {
+    name: "Jack Lee",
+    key: "Jack",
+    text: "I'm blown away by the quality of service at PulseFit. The Elite Membership's benefits, including unlimited personal training sessions and exclusive workshops, have elevated my fitness journey to new heights. The amenities are top-notch",
+    image: "/images/man4.jpg",
+    icon: IoMdQuote,
+  },
+  {
+    name: "Emily Boss",
+    key: "Emily",
+    text: "PulseFit's CoreBlast Bootcamp is my favorite! The focused core workouts have strengthened my abs and back, and the Basic Membership's inclusion of a weekly class is perfect for staying on track with my goals",
+    image: "/images/woman5.jpg",
+    icon: IoMdQuote,
+  },
+  {
+    name: "Liam Roberts",
+    key: "Liam",
+    text: "The variety of classes at PulseFit ensures I stay engaged and challenged. From Boxing Burn to Pilates Fusion, the Premium Membership's unlimited access is worth every penny. Plus, the 10% discount on merchandise is a nice bonus!",
+    image: "/images/man5.jpg",
+    icon: IoMdQuote,
+  },
+  {
+    name: "Olivia Wilson",
+    key: "Olivia",
+    text: "PulseFit's commitment to wellness is evident in its exceptional recovery amenities. The advanced recovery tools and personalized nutrition plans included with my Elite Membership have made a huge difference in my overall well-being.",
+    image: "/images/woman6.jpg",
+    icon: IoMdQuote,
+  },
+  {
+    name: "Ryan Mitchell",
+    key: "Ryan",
+    text: "The community at PulseFit is incredibly supportive. With my Basic Membership, I've enjoyed trying different classes and meeting new people. The friendly atmosphere and high-quality equipment make it a fantastic place to work out.",
+    image: "/images/man6.jpg",
+    icon: IoMdQuote,
   },
 ];
