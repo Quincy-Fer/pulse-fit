6c6857364f8fbb629f73b282efd4ccdc0d289350
Quincy Fer <quincy.e.fer@gmail.com>
2024-08-16 14:03:58 +0200

    Add hover effects, small styling changes

--- components/ClassCard.tsx
+++ components/ClassCard.tsx
@@ -20,13 +20,13 @@ const ClassCard = ({
   return (
     <div
       className="relative w-full h-[300px] lg:h-[485px] flex
-              justify-center items-center"
+              justify-center items-center hover:border-4 hover:border-red-700 transition-all "
     >
-      <div className="bg-black/55 absolute w-full h-full top-0 z-10"></div>
+      <div className="bg-black/60 absolute w-full h-full top-0 z-10"></div>
       <Image src={image} fill={true} className="object-cover" alt={alt} />
       <div className="z-10 flex flex-col justify-around ">
         <h4 className="text-white text-center text-4xl ">{title}</h4>
-        <p className="text-white max-w-[600px] text-lg  mt-12 text-center">
+        <p className="text-white max-w-[600px] text-lg  mt-12 text-center tracking-[3px]">
           {paragraph}
         </p>
 
@@ -34,7 +34,7 @@ const ClassCard = ({
           href="/classes"
           className="flex gap-2 mt-10 justify-center transition-all "
         >
-          <p className="text-white underline underline-offset-8 font-bold text-xl uppercase mt-1 ">
+          <p className="text-white underline underline-offset-8 font-bold text-base uppercase mt-1 ">
             learn more
           </p>
           <Icon className="text-accent text-4xl " />
