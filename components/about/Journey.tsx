4f23078360cb9f7cbaffcd6182247f1974588762
Quincy Fer <quincy.e.fer@gmail.com>
2024-08-20 15:37:24 +0200

    Finish mission and journey part

--- components/about/Journey.tsx
+++ components/about/Journey.tsx
@@ -1,5 +1,29 @@
+import { SUBPAGE_ABOUT } from "@/constants";
+import Image from "next/image";
+
 const Journey = () => {
-  return <div>Journey</div>;
+  return (
+    <section className="mx-auto  p-16">
+      <div className="flex gap-20">
+        <div className="flex flex-col">
+          <h3 className="text-3xl mb-4">Our Journey So Far</h3>
+
+          <p className="max-w-[600px] leading-loose text-gray-800 ">
+            {SUBPAGE_ABOUT.journey}
+          </p>
+        </div>
+        <div>
+          <Image
+            src="/images/journey.jpg"
+            width={500}
+            height={1000}
+            alt="Image of gym"
+          />
+        </div>
+      </div>
+      <div className="border-b-2 border-dashed border-accent mt-16"></div>
+    </section>
+  );
 };
 
 export default Journey;
--- components/about/Mission.tsx
+++ components/about/Mission.tsx
@@ -1,5 +1,28 @@
+import { SUBPAGE_ABOUT } from "@/constants";
+import Image from "next/image";
+
 const Mission = () => {
-  return <div>Mission</div>;
+  return (
+    <section className="mx-auto  ">
+      <div className="flex gap-20 ">
+        <div>
+          <Image
+            src="/images/mission.jpg"
+            width={500}
+            height={1000}
+            alt="Image of gym"
+          />
+        </div>
+        <div className="flex flex-col">
+          <h3 className="text-3xl mb-4">Our Mission, Your Success</h3>
+
+          <p className="max-w-[600px] leading-loose text-gray-800 ">
+            {SUBPAGE_ABOUT.mission}
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Mission;
