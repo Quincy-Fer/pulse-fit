3de92ad50ba79ecb20b82df16bce7712454b765c
Quincy Fer <quincy.e.fer@gmail.com>
2024-08-21 12:55:40 +0200

    Create skeleton files for classes

--- components/about/Trainers.tsx
+++ components/about/Trainers.tsx
@@ -5,8 +5,8 @@ const Trainers = () => {
   return (
     <section className="mx-auto pb-16">
       <div className="border-t-2 border-dashed border-accent mt-16"></div>
-      <h3 className="text-3xl mt-8">Our Trainers</h3>
-      <p className="tracking-[1px] uppercase text-base mt-2 text-gray-800">Hover To Find Out More</p>
+      <h3 className="text-3xl mt-8 text-center">Our Trainers</h3>
+      <p className="tracking-[1px] uppercase text-base mt-2 text-gray-800 text-center">Hover To Find Out More</p>
       <div className="grid grid-cols-3 gap-4 mt-4">
         {SUBPAGE_ABOUT_TRAINERS.map((item) => {
           return <TrainersCard {...item} />;
