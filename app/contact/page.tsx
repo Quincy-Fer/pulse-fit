92c6cb1f7e2cc2a711e1875eaa212fd2bba81536
Quincy Fer <quincy.e.fer@gmail.com>
2024-08-23 13:55:57 +0200

    Edit styling contactform, add contact information

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,7 @@
+import ContactForm from "@/components/contact/ContactForm";
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { FaEnvelope, FaPhone } from "react-icons/fa6";
+
 const page = () => {
   return (
     <section className="bg-slate-100 mx-auto ">
@@ -6,7 +10,32 @@ const page = () => {
       </div>
 
       <div className="p-16">
-        <h3 className="text-3xl text-accent uppercase font-bold">Send us a Message</h3>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-16">
+          <ContactForm />
+          {/* contact section */}
+          <div className="flex flex-col">
+            <h3 className="text-3xl  uppercase font-bold mb-10">
+              Find us at: 
+            </h3>
+            <div className="flex gap-2 mb-4 ">
+              <FaMapMarkerAlt className="text-2xl text-accent" />
+              <p className="text-lg text-gray-800">Elysium 1 , FlavorTown</p>
+            </div>
+            <div className="flex gap-2 mb-4 ">
+              <FaPhone className="text-2xl text-accent" />
+              <p className="text-lg text-gray-800">123-456-789</p>
+            </div>
+            <div className="flex gap-2 ">
+              <FaEnvelope className="text-2xl text-accent" />
+              <a
+                href="mailto:pulsefit_demo@gmail.com"
+                className="text-lg hover:text-accent transition-all text-gray-800"
+              >
+                pulsefit_demo@gmail.com
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
--- components/contact/ContactForm.tsx
+++ components/contact/ContactForm.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const ContactForm = () => {
   return (
-    <div className="max-w-2xl mx-auto p-8">
-      <h2 className="text-3xl font-bold mb-6 text-center">Get in Touch with PulseFit</h2>
+    <div className="max-w-2xl ">
+      <h3 className="text-3xl  uppercase font-bold mb-8">
+        Send us a Message !
+      </h3>
       <form className="space-y-6">
         <div className="flex flex-col md:flex-row md:space-x-4">
           <div className="flex-1">
-            <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+            <label
+              htmlFor="name"
+              className="block text-base font-bold text-gray-700"
+            >
               Name
             </label>
             <input
@@ -18,7 +23,10 @@ const ContactForm = () => {
             />
           </div>
           <div className="flex-1 mt-4 md:mt-0">
-            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+            <label
+              htmlFor="email"
+              className="block text-base font-bold text-gray-700"
+            >
               Email
             </label>
             <input
@@ -31,7 +39,10 @@ const ContactForm = () => {
         </div>
 
         <div>
-          <label htmlFor="subject" className="block text-sm font-medium text-gray-700">
+          <label
+            htmlFor="subject"
+            className="block text-base font-bold text-gray-700"
+          >
             Subject
           </label>
           <input
@@ -43,21 +54,23 @@ const ContactForm = () => {
         </div>
 
         <div>
-          <label htmlFor="message" className="block text-sm font-medium text-gray-700">
+          <label
+            htmlFor="message"
+            className="block text-base font-bold text-gray-700"
+          >
             Message
           </label>
           <textarea
             id="message"
-            rows="5"
-            className="mt-1 p-2 w-full border border-gray-300 rounded-md shadow-sm focus:ring-accent focus:border-accent"
+            className="mt-1 p-2 w-full border resize-none border-gray-300 rounded-md shadow-sm focus:ring-accent focus:border-accent"
             placeholder="Your Message"
           ></textarea>
         </div>
 
         <div className="text-center">
           <button
-            type="submit"
-            className="px-6 py-2 bg-accent text-white font-semibold rounded-md hover:bg-accent-dark transition-all"
+            type="button"
+            className="px-6 py-2 bg-accent text-white font-semibold hover:bg-black transition-all"
           >
             Send Message
           </button>
