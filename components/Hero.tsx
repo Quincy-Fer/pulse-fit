0f9d0d56a1fccf71512b6a96dff90e0fa7a145d9
Quincy Fer <quincy.e.fer@gmail.com>
2024-09-04 15:46:48 +0200

    adjust text and margins for hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,11 +9,11 @@ const Hero = () => {
     >
       <div className="w-full h-full bg-black/40 absolute top-0 z-10">
         <div className="absolute top-16 p-16">
-          <h1 className="text-white text-7xl tracking-tight leading-tight max-w-[800px] text-balance font-bold">
+          <h1 className="text-white md:text-6xl lg:text-7xl tracking-tight leading-tight md:max-w-[500px] lg:max-w-[800px] text-balance font-bold">
             Where every <span className="text-accent">Pulse</span> powers your
             <span className="text-red-700"> Potential</span>
           </h1>
-          <p className="text-white text-lg max-w-[600px] mt-5 leading-relaxed">
+          <p className="text-white text-lg max-w-[600px] md:mt-14 lg:mt-5 leading-relaxed">
             {HERO.paragraph}
           </p>
           <div className="flex gap-20 mt-16">
