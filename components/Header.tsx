7df895fcfdb7e0ec6b95688bd8dbfd588c3d2058
Quincy Fer <quincy.e.fer@gmail.com>
2024-09-04 15:02:39 +0200

    Add mobile navigation, add hamburger and closing buttons

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,20 @@
+'use client'
+
+import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { NAVLINKS } from "@/constants/index";
 import ButtonJoin from "./ButtonJoin";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <header
-      className="fixed flex items-center justify-between px-14  z-30 py-2  w-full mx-auto  bg-black"
-      id="home"
-    >
+    <header className="fixed flex items-center justify-between px-6 lg:px-14 z-30 py-2 w-full mx-auto bg-black" id="home">
       {/* Logo */}
       <Link href="/">
         <Image
@@ -20,21 +26,75 @@ const Header = () => {
           className="py-1"
         />
       </Link>
+
+      {/* Hamburger Icon for Mobile */}
+      <button
+        className="lg:hidden text-white focus:outline-none"
+        onClick={toggleMenu}
+        aria-label="Toggle Menu"
+      >
+        <svg
+          className="w-6 h-6"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
+          ></path>
+        </svg>
+      </button>
+
       {/* Links Desktop */}
-      <nav className="flex gap-4 ">
-        {NAVLINKS.map((item) => {
-          return (
-            <Link
-              key={item.key}
-              href={item.href}
-              className="text-white uppercase hover:text-accent transition-all font-bold  text-base"
-            >
-              {item.label}
-            </Link>
-          );
-        })}
+      <nav className="hidden lg:flex gap-4">
+        {NAVLINKS.map((item) => (
+          <Link
+            key={item.key}
+            href={item.href}
+            className="text-white uppercase hover:text-accent transition-all font-bold text-base"
+          >
+            {item.label}
+          </Link>
+        ))}
       </nav>
-      <div className="">
+
+      {/* Mobile Menu */}
+      {isOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-95 z-20 flex flex-col items-center justify-center lg:hidden">
+          {/* Close Button */}
+          <button
+            className="absolute top-4 right-4 text-white text-3xl focus:outline-none"
+            onClick={toggleMenu}
+            aria-label="Close Menu"
+          >
+            &times;
+          </button>
+
+          <nav className="flex flex-col items-center">
+            {NAVLINKS.map((item) => (
+              <Link
+                key={item.key}
+                href={item.href}
+                className="text-white uppercase hover:text-accent transition-all font-bold text-base py-4"
+                onClick={toggleMenu} // Close menu after click
+              >
+                {item.label}
+              </Link>
+            ))}
+          </nav>
+          <ButtonJoin
+            href="/membership"
+            text="Join Now"
+            containerStyles="w-full text-center py-4 px-4 text-white bg-accent uppercase font-bold hover:bg-black hover:text-accent transition-all duration-300 mt-4"
+          />
+        </div>
+      )}
+
+      <div className="hidden lg:block">
         <ButtonJoin
           href="/membership"
           text="Join Now"
