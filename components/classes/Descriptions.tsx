04853f94301f051930086877799f88d4a9d715b6
Quincy Fer <quincy.e.fer@gmail.com>
2024-10-07 15:50:09 +0200

    add margins between images and text, class section

--- components/classes/Descriptions.tsx
+++ components/classes/Descriptions.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 const Descriptions = () => {
   return (
     <section className="px-16 pt-16">
-      <h3 className="text-3xl mb-2 text-center w-full ">What We Offer</h3>
-      <p className="uppercase tracking-[1px] mb-16 text-center text-gray-700  ">
+      <h3 className="text-3xl mb-2 lg:text-center w-full ">What We Offer</h3>
+      <p className="uppercase tracking-[1px] mb-16 lg:text-center text-gray-700  ">
         take a closer look at our wide variety of classes
       </p>
       {/* 1st category */}
@@ -13,7 +13,7 @@ const Descriptions = () => {
         {SUBPAGE_CLASSES_DESCRIPTIONS[0].title}
       </h4>
 
-      <div className="grid grid-cols-1 lg:grid-cols-2">
+      <div className="grid grid-cols-1 lg:grid-cols-2 ">
         <div className="flex flex-col gap-8">
           <div>
             <h5 className="text-lg mb-2">
@@ -27,12 +27,12 @@ const Descriptions = () => {
             <h5 className="text-lg mb-2">
               {SUBPAGE_CLASSES_DESCRIPTIONS[0].subtitle2}
             </h5>
-            <p className="text-gray-800 leading-relaxed max-w-[600px]">
+            <p className="text-gray-800 leading-relaxed max-w-[600px] md:max-lg:mb-4">
               {SUBPAGE_CLASSES_DESCRIPTIONS[0].paragraph2}
             </p>
           </div>
         </div>
-        <div className="relative border border-black h-[420px] w-[600px]">
+        <div className="relative border border-black h-[420px] w-[600px] ">
           <Image
             src={SUBPAGE_CLASSES_DESCRIPTIONS[0].image}
             alt={SUBPAGE_CLASSES_DESCRIPTIONS[0].alt}
@@ -45,7 +45,7 @@ const Descriptions = () => {
         {SUBPAGE_CLASSES_DESCRIPTIONS[1].title}
       </h4>
       <div className="grid grid-cols-1 lg:grid-cols-2">
-        <div className="relative border border-black h-[420px] w-[600px]">
+        <div className="relative border border-black h-[420px] w-[600px] md:max-lg:mb-4">
           <Image
             src={SUBPAGE_CLASSES_DESCRIPTIONS[1].image}
             alt={SUBPAGE_CLASSES_DESCRIPTIONS[1].alt}
@@ -89,7 +89,7 @@ const Descriptions = () => {
             <h5 className="text-lg mb-2">
               {SUBPAGE_CLASSES_DESCRIPTIONS[2].subtitle2}
             </h5>
-            <p className="text-gray-800 leading-relaxed max-w-[600px]">
+            <p className="text-gray-800 leading-relaxed max-w-[600px] md:max-lg:mb-4">
               {SUBPAGE_CLASSES_DESCRIPTIONS[2].paragraph2}
             </p>
           </div>
@@ -107,7 +107,7 @@ const Descriptions = () => {
         {SUBPAGE_CLASSES_DESCRIPTIONS[3].title}
       </h4>
       <div className="grid grid-cols-1 lg:grid-cols-2">
-        <div className="relative border border-black h-[420px] w-[600px]">
+        <div className="relative border border-black h-[420px] w-[600px] md:max-lg:mb-4">
           <Image
             src={SUBPAGE_CLASSES_DESCRIPTIONS[3].image}
             alt={SUBPAGE_CLASSES_DESCRIPTIONS[3].alt}
@@ -151,7 +151,7 @@ const Descriptions = () => {
             <h5 className="text-lg mb-2">
               {SUBPAGE_CLASSES_DESCRIPTIONS[4].subtitle2}
             </h5>
-            <p className="text-gray-800 leading-relaxed max-w-[600px]">
+            <p className="text-gray-800 leading-relaxed max-w-[600px] md:max-lg:mb-4">
               {SUBPAGE_CLASSES_DESCRIPTIONS[4].paragraph2}
             </p>
           </div>
@@ -169,7 +169,7 @@ const Descriptions = () => {
         {SUBPAGE_CLASSES_DESCRIPTIONS[5].title}
       </h4>
       <div className="grid grid-cols-1 lg:grid-cols-2">
-        <div className="relative border border-black h-[420px] w-[600px]">
+        <div className="relative border border-black h-[420px] w-[600px] md:max-lg:mb-4">
           <Image
             src={SUBPAGE_CLASSES_DESCRIPTIONS[5].image}
             alt={SUBPAGE_CLASSES_DESCRIPTIONS[5].alt}
